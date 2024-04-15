a394a147b9e39d90972b133aa2866f9db54c2033
Luan Oliveira Peralta <opluan.dev@gmail.com>
2024-04-14 21:24:01 -0300

    chore: setup gha deploy for gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://opluan.github.io',
 	server: { host: true},
 	integrations: [
 		starlight({
